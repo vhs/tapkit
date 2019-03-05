1373a277f5b995c60e2064523d10edf1de29573b
Thomas LaFreniere <thomas@lafreniere.co>
2019-03-04 20:56:38 -0800

    nodejs example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tapkit
 IoT package + SDK for NFC tap setup to bootstrap member card tap integrations
+
+# nodejs example
+
+https://github.com/vhs/tapkit/master/src/node
---
+++ src/node/index.js
@@ -0,0 +1,25 @@
+const nci = require("node-nfc-nci");
+const https = require('https');
+
+const NOMOS_BASE_URI = process.env.NOMOS_BASE_URI || "https://membership.vanhack.ca/services/web";
+const NOMOS_API_KEY = process.env.NOMOS_API_KEY || "";
+
+nci.listen(context => {
+  context.on("error", msg => console.error(msg));
+  
+  context.on("arrived", tag => {
+    https.get(`${NOMOS_BASE_URI}/AuthService1.svc/CheckRfid?rfid=${tag.uid.id}`, {
+      headers: { "X-Api-Key": NOMOS_API_KEY }
+    }, (resp) => {
+      let data = "";
+      
+      resp.on("data", chunk => {
+        data += chunk;
+      });
+      
+      resp.on("end", () => {
+        console.log(JSON.parse(data).explaination);
+      });
+    }).on("error", err => console.error(err.message));
+  });
+});
